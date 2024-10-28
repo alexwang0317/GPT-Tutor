da3b4ffdbeb0c9daf333d84cbfa550a48e2e4afe
alexwang0317 <87534962+alexwang0317@users.noreply.github.com>
2024-10-28 10:18:55 -0700

    model changes to gpt-4o-mini

--- src/app/api/explain/explain.js
+++ src/app/api/explain/explain.js
@@ -1,24 +1,23 @@
 // pages/api/explain.js
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 export default async function handler(req, res) {
   const { text } = req.body;
 
-  const configuration = new Configuration({
+  const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const openai = new OpenAIApi(configuration);
 
   try {
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-4o-mini', // Use 'gpt-4' if you have access
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4o-mini', // 
       messages: [
         { role: 'system', content: 'You are an assistant that explains text for academic setting and hopes to provide a thorough explanation.' },
         { role: 'user', content: `Please explain the following text in simple and digestable terms, use analogies and examples to help the user understand:\n\n"${text}"` },
       ],
     });
 
-    const explanation = completion.data.choices[0].message.content;
+    const explanation = completion.choices[0].message.content;
     res.status(200).json({ explanation });
   } catch (error) {
     console.error('Error fetching explanation:', error);
